4612f3f32cbe96c1c7927787d9ce6d84c4e65ce1
terjirapat <terjrp1997@gmail.com>
2026-03-13 09:51:25 +0700

    reset

--- package/embedding/model.py
+++
@@ -1,9 +0,0 @@
-from sentence_transformers import SentenceTransformer
-
-class SimpleEmbedding:
-    def __init__(self):
-        self.model = SentenceTransformer('sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
-    
-    def run(self, sentence:list):
-        embeddings = self.model.encode(sentence)
-        return embeddings
